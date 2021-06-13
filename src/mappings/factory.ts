cae10e95c94dbf994e8de9cdbc80ce3611baea93
Tran Huy Duan <tranhuy.duan@fusang.co>
2021-06-13 22:45:36 +0700

    Event burn mint transfer handle

--- src/mappings/factory.ts
+++ src/mappings/factory.ts
@@ -6,19 +6,13 @@ import * as Utils from '../utils'
 
 
 export function handlePairCreated(event: PairCreated): void {
-  log.debug('error 1', [])
   let factory = Account.load(Utils.FactoryAddress)
-  log.debug('error 2', [])
   if (factory === null) {
     factory = new Account(Utils.FactoryAddress)
     factory.save()
   }
-  // create the tokens
   let token0 = Token.load(event.params.token0.toHexString())
   let token1 = Token.load(event.params.token1.toHexString())
-  log.debug('error 3', [])
-
-  // fetch info if null
   if (token0 === null) {
     token0 = new Token(event.params.token0.toHexString())
     token0.blockNumber = event.block.number
@@ -27,14 +21,11 @@ export function handlePairCreated(event: PairCreated): void {
     token0.name = Utils.getTokenName(event.params.token0)
     token0.tokenStandard = 'ERC20'
     let decimals = Utils.getTokenDecimals(event.params.token0)
-    // bail if we couldn't figure out the decimals
     if (decimals === null) {
       log.debug('Decimal is null', [])
       return
     }
   }
-  log.debug('error 4', [])
-  // fetch info if null
   if (token1 === null) {
     token1 = new Token(event.params.token1.toHexString())
     token1.blockNumber = event.block.number
@@ -43,13 +34,11 @@ export function handlePairCreated(event: PairCreated): void {
     token1.name = Utils.getTokenName(event.params.token1)
     token1.tokenStandard = 'ERC20'
     let decimals = Utils.getTokenDecimals(event.params.token1)
-    // bail if we couldn't figure out the decimals
     if (decimals === null) {
       log.debug('Decimal is null', [])
       return
     }
   }
-  log.debug('error 5', [])
   let pair = new Pair(event.params.pair.toHexString()) as Pair
   pair.token0 = token0.id
   pair.token1 = token1.id
@@ -59,10 +48,10 @@ export function handlePairCreated(event: PairCreated): void {
   pair.totalSupply = BigInt.fromI32(0)
   pair.blockNumber = event.block.number
   pair.timestamp = event.block.timestamp
-  log.debug('error 6', [])
   PairTemplate.create(event.params.pair)
   token0.save()
   token1.save()
   pair.save()
-  log.debug('error 7', [])
+  Utils.createPairSnapshot(pair, event)
+  Utils.createMarket(pair)
 }
--- src/mappings/pairs.ts
+++ src/mappings/pairs.ts
@@ -1,72 +1,155 @@
+import { log, BigInt, ethereum } from '@graphprotocol/graph-ts'
 import { Approval, Transfer } from "../../generated/Factory/ERC20"
-import { Burn, Mint } from "../../generated/schema"
+import { Burn, Mint, Pairs } from "../../generated/templates/Pairs/Pairs"
+import { Transaction, Burn as BurnEntity, Mint as MintEntity, Token, Pair, Account, Market } from "../../generated/schema"
+import { bigIntToDecimal, createAccount, createMarket, createPairSnapshot, createPosition, createPositionSnapshot, FactoryAddress, ONE, updatePosition, ZERO, ZeroAddress } from '../utils'
 
+export function handleBurn(event: Burn): void { 
+  let transaction = Transaction.load(event.transaction.hash.toHexString()) as Transaction
+  let burn = BurnEntity.load(event.transaction.hash.toHexString()) as BurnEntity
 
-export function handleApproval(event: Approval): void {
-//   // Entities can be loaded from the store using a string ID; this ID
-//   // needs to be unique across all entities of the same type
-//   let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let pair = Pair.load(event.address.toHex())
 
-//   // Entities only exist after they have been saved to the store;
-//   // `null` checks allow to create entities on demand
-//   if (entity == null) {
-//     entity = new ExampleEntity(event.transaction.from.toHex())
-
-//     // Entity fields can be set using simple assignments
-//     entity.count = BigInt.fromI32(0)
-//   }
-
-//   // BigInt and BigDecimal math are supported
-//   entity.count = entity.count + BigInt.fromI32(1)
-
-//   // Entity fields can be set based on event parameters
-//   entity.owner = event.params.owner
-//   entity.spender = event.params.spender
-
-//   // Entities can be written to the store with `.save()`
-//   entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DOMAIN_SEPARATOR(...)
-  // - contract.MINIMUM_LIQUIDITY(...)
-  // - contract.PERMIT_TYPEHASH(...)
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.burn(...)
-  // - contract.decimals(...)
-  // - contract.factory(...)
-  // - contract.getReserves(...)
-  // - contract.kLast(...)
-  // - contract.mint(...)
-  // - contract.name(...)
-  // - contract.nonces(...)
-  // - contract.price0CumulativeLast(...)
-  // - contract.price1CumulativeLast(...)
-  // - contract.symbol(...)
-  // - contract.token0(...)
-  // - contract.token1(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
+  let token0 = Token.load(pair.token0)
+  let token1 = Token.load(pair.token1)
+  let account = Account.load(transaction.from) as Account
+  let token0Input = token0.id
+  .concat('-')
+  .concat(burn.to).concat('-').concat(event.params.amount0.toHexString())
+  let token1Input = token1.id
+  .concat('-')
+  .concat(burn.to).concat('-').concat(event.params.amount1.toHexString())
+  transaction.inputTokenAmounts = [token0Input, token1Input]
+  burn.amount0 = event.params.amount0
+  burn.amount1 = event.params.amount1
+  burn.burnEventApplied = true
+  burn.save()
+  let position = createPosition(account, pair.id, 'DEBT', transaction)
+  position.save()
+  createPositionSnapshot(position, transaction)
+  position.historyCounter = position.historyCounter.plus(ONE)
+  position.save()
 }
 
-export function handleBurn(event: Burn): void {}
+export function handleMint(event: Mint): void {
+  let transaction = Transaction.load(event.transaction.hash.toHexString()) as Transaction
+  let mint = MintEntity.load(event.transaction.hash.toHexString()) as MintEntity
 
-export function handleMint(event: Mint): void {}
+  let pair = Pair.load(event.address.toHex())
 
-export function handleTransfer(event: Transfer): void {}
+  let token0 = Token.load(pair.token0)
+  let token1 = Token.load(pair.token1)
+  let account = Account.load(transaction.from) as Account
+  let token0Input = token0.id
+  .concat('-')
+  .concat(mint.to).concat('-').concat(event.params.amount0.toHexString())
+  let token1Input = token1.id
+  .concat('-')
+  .concat(mint.to).concat('-').concat(event.params.amount1.toHexString())
+  transaction.inputTokenAmounts = [token0Input, token1Input]
+  mint.amount0 = event.params.amount0
+  mint.amount1 = event.params.amount1
+  mint.mintEventApplied = true
+  mint.save()
+  let position = createPosition(account, pair.id, 'INVESTMENT', transaction)
+  position.save()
+  createPositionSnapshot(position, transaction)
+  position.historyCounter = position.historyCounter.plus(ONE)
+  position.save()
+ }
+
+export function handleSwap(event: ethereum.Event): void { 
+// TODO: Update the position 
+let pair = Pair.load(event.address.toHexString())
+let token0 = Token.load(pair.token0)
+let token1 = Token.load(pair.token1)
+}
+
+export function handleSync(event: ethereum.Event): void {
+// TODO: Update the position 
+let pair = Pair.load(event.address.toHexString())
+let token0 = Token.load(pair.token0)
+let token1 = Token.load(pair.token1)
+ }
+
+export function handleTransfer(event: Transfer): void {
+  if (event.params.to.toHexString() == ZeroAddress && event.params.value.equals(BigInt.fromI32(1000))) {
+    return
+  }
+  let transactionHash = event.transaction.hash.toHexString()
+  let from = event.params.from
+  let sender = createAccount(from.toHexString())
+  let to = event.params.to
+  let receiver = createAccount(to.toHexString())
+  let pair = Pair.load(event.address.toHexString()) as Pair
+  let market = Market.load(event.address.toHexString()) as Market
+
+  let pairContract = Pairs.bind(event.address)
+  let transaction = Transaction.load(transactionHash) as Transaction
+  if (transaction === null) {
+    transaction = new Transaction(transactionHash)
+    transaction.from = sender.id
+    transaction.to = receiver.id
+    transaction.blockNumber = event.block.number
+    transaction.transactionType = 'INVEST'
+    transaction.timestamp = event.block.timestamp
+    transaction.gasUsed = event.transaction.gasUsed
+    transaction.gasPrice = event.transaction.gasPrice
+    transaction.timestamp = event.block.timestamp
+    transaction.transactionIndexInBlock = event.transaction.index
+    transaction.market = market.id
+    transaction.outputTokenAmount = ZERO
+    transaction.inputTokenAmounts = []
+    transaction.rewardTokenAmounts = []
+  }
+  if (from.toHexString() == ZeroAddress) {
+    pair.totalSupply = pair.totalSupply.plus(event.params.value)
+    pair.save()
+    createPairSnapshot(pair, event)
+    let mint = MintEntity.load(transactionHash)
+    if (mint === null) {
+      mint = new MintEntity(transactionHash)
+      mint.pair = pair.id
+      mint.to = receiver.id
+      mint.liquityAmount = event.params.value
+      mint.transferEventApplied = true
+      mint.syncEventApplied = false
+      mint.mintEventApplied = false
+      mint.save()
+    }
+  }
+  if (to.toHexString() == ZeroAddress && from.toHexString() == pair.id) {
+    pair.totalSupply = pair.totalSupply.minus(event.params.value)
+    pair.save()
+    createPairSnapshot(pair, event)
+    let burn = BurnEntity.load(transactionHash)
+    if (burn === null) {
+      burn = new BurnEntity(transactionHash)
+      burn.pair = pair.id
+      burn.to = sender.id
+      burn.liquityAmount = event.params.value
+      burn.transferEventApplied = true
+      burn.syncEventApplied = false
+      burn.burnEventApplied = false
+      burn.save()
+    }
+  }
+  if (from.toHexString() != ZeroAddress && from.toHexString() != pair.id) {
+    let fromUserPosistion = createPosition(sender, pair.id, 'INVESTMENT', transaction)
+    fromUserPosistion.outputTokenBalance = pairContract.balanceOf(from)
+    fromUserPosistion.save()
+    createPositionSnapshot(fromUserPosistion, transaction)
+    fromUserPosistion.historyCounter = fromUserPosistion.historyCounter.plus(ONE)
+    fromUserPosistion.save()
+  }
+
+  if (event.params.to.toHexString() != ZeroAddress && to.toHexString() != pair.id) {
+    let toUserPosition = createPosition(receiver, pair.id, 'INVESTMENT', transaction)
+    toUserPosition.outputTokenBalance = pairContract.balanceOf(to)
+    toUserPosition.save()
+    createPositionSnapshot(toUserPosition, transaction)
+    toUserPosition.historyCounter = toUserPosition.historyCounter.plus(ONE)
+    toUserPosition.save()
+  }
+  transaction.save()
+}
